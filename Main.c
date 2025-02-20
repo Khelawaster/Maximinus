330489af475a0aa816f6af06fa3e2e1b1be8b8aa
Khelawaster <khelawasterofancardia@gmail.com>
2025-02-20 11:38:47 +0300

    Task0OptionsDat

--- Main.c
+++ Main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
+#include <stdlib.h>
 
 /*
 13/01/2025
@@ -23,9 +25,34 @@
 Не требуя прав администратора
 */
 
+
 int intSetting;
 double dblSetting;
-char strSetting [];
+char * strSetting;
+
+int ParseSettingString(char* string, char * name, char * value) {
+	if (string[0] == ';') return 0;
+	char* ch = name;
+	int j = 0;
+	for (int i=0 ;i< strlen(string);i++, j++)
+	{
+		if(string[i]=='=') 
+		{
+			ch[j] = '\0';
+			ch = value;
+			j = -1;
+			continue;
+		}
+		else if (string[i] == '\n')
+		{
+			ch[j] = '\0';
+			break;
+		}
+		else
+			ch[j] = string[i];		 
+	}
+	return 1;
+}
 
 void SettingsCreate()
 {
@@ -36,9 +63,18 @@ void SettingsCreate()
 	*/
 	FILE* fp = fopen("options.dat", "w"); //текстовый файл открывается для записи. 
 	//Если файл ранее существовал, то он пересоздается и записывается заново
-
-
-	
+	if (fp == NULL)
+	{
+		printf("Ой всё сломалось!!!\nНе могу создать файл конфигурации.");
+		_fcloseall();
+		_exit(1);
+	}
+	fputs(";Файл конфигурации программы Maximinus\n", fp);
+	fputs(";Внимание! Длина любого параметра не должна превышать 128 символов!\n", fp);
+	fputs(";intSetting\nintSetting=1\n", fp);
+	fputs(";dblSetting Разделитель целой и дробной частей - запятая.\ndblSetting=0,1\n", fp);
+	fputs(";strSetting\nstrSetting=setting value is 0.1\n", fp);
+	fclose(fp);
 }
 
 
@@ -50,21 +86,37 @@ void SettingsRead()
 		fp = fopen("options.dat", "r"); //текстовый файл открывается для чтения
 		if (fp == NULL)
 		{
-			//добавить сообщение на консоль
+			printf("Файл конфигурации не найден и будет восстановлен со значениями по умолчанию.\n");
 			SettingsCreate();
 			continue;
 		}
 		break;
 	}
-
-
-
-
+	char buffer[257];
+	char settingName[128];
+	char settingValue[128];
+	while ((fgets(buffer, 256, fp)) != NULL)
+	{
+		if (ParseSettingString(buffer, settingName, settingValue) == 1)
+		{
+			//printf("%s\t%s\n", settingName, settingValue);
+			if (strcmp(settingName, "intSetting") ==0)
+				intSetting = atoi(settingValue);
+			else if (strcmp(settingName, "dblSetting") == 0) 
+				dblSetting = atof(settingValue);
+			else if  (strcmp(settingName, "strSetting") == 0) 
+				strSetting = settingValue;
+		}
+	}
+	printf("%d\t%f\t%s\n", intSetting, dblSetting, strSetting);
 	fclose(fp);
 	return 0;
 }
 
+
 int main() {
-	//printf("AVE CAESAR");
-	ReadSettings();
+	char* locale = setlocale(LC_ALL, ""); //локализация
+	SettingsRead();
+	printf("AVE CAESAR");
+	getchar();
 }
